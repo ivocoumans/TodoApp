6cb34845f0f8f21d5b71bfc002d52e30ff9f067e
Ivo Coumans <1718078+ivocoumans@users.noreply.github.com>
2023-04-05 10:22:26 +0200

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Ivo Coumans on 05/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
